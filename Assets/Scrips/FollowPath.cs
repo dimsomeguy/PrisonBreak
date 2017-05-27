8da94da6326f6cfbd4ad84ba03d6ad54902be8fb
Dale Taylor <JustSomeGuy637@gmail.com>
2017-05-27 02:05:30 +0100

    basic level
    stamina bar and basic pathing

---
+++ Assets/Scrips/FollowPath.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour {
+
+    public enum MovementType
+    {
+        MoveTowards,
+        LerpTowards
+    }
+
+
+    public MovementType Type = MovementType.MoveTowards;
+    public MovementPath MyPath;
+    public float Speed = 1f;
+    public float MaxDistenceToGoal = .1f;
+
+    private IEnumerator<Transform> PointInPath;
+	// Use this for initialization
+	void Start () {
+        if (MyPath == null) {
+            Debug.Log("there is no path to follow please assind to");
+            return;
+        }
+
+        PointInPath = MyPath.GetNextPathPoint();
+
+        PointInPath.MoveNext();
+
+        if (PointInPath.Current == null)
+        {
+            Debug.LogError("A Path must ahve points in it to follow", gameObject);
+            return;
+        }
+
+        transform.position = PointInPath.Current.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (PointInPath == null || PointInPath.Current == null)
+        {
+            return;
+        }
+
+        if (Type == MovementType.MoveTowards)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PointInPath.Current.position, Time.deltaTime * Speed);
+        }
+        else if (Type == MovementType.LerpTowards)
+        {
+            transform.position = Vector3.Lerp(transform.position, PointInPath.Current.position, Time.deltaTime * Speed);
+        }
+
+        float distence = (transform.position - PointInPath.Current.position).sqrMagnitude;
+
+        if (distence < MaxDistenceToGoal * MaxDistenceToGoal)
+        {
+            PointInPath.MoveNext();
+        }
+	}
+}
---
+++ Assets/Scrips/MovementPath.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPath : MonoBehaviour {
+
+    public enum PathTypes
+    {
+        linear,
+        loop
+    }
+
+    public PathTypes type;
+    public int movementDirection = 1;
+    public int movingTo = 0;
+    public Transform[] PathSeq;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //draw them gizmos
+    public void OnDrawGizmos()
+    {
+        if (PathSeq == null || PathSeq.Length < 2)
+            return;
+
+        for (int i = 1; i < PathSeq.Length; i++)
+        {
+            Gizmos.DrawLine(PathSeq[i -1].position, PathSeq[i].position);
+        }
+
+        if (type == PathTypes.loop)
+        {
+            Gizmos.DrawLine(PathSeq[0].position, PathSeq[PathSeq.Length - 1].position);
+        }
+    }
+
+    //get next point of transofrm for pathing
+    public IEnumerator<Transform> GetNextPathPoint()
+    {
+        //does the object have a seq
+        if (PathSeq == null || PathSeq.Length < 1)
+            yield break;
+        //if not or it is a stachatory position return
+
+
+        while (true)
+        {
+            yield return PathSeq[movingTo];
+
+            if (PathSeq.Length == 1)
+            {
+                continue;
+            }
+
+            if (type == PathTypes.linear)
+            {
+                if (movingTo <= 0)
+                    movementDirection = 1;
+                else if (movingTo >= PathSeq.Length - 1)
+                    movementDirection = -1;
+                
+                movingTo = movingTo + movementDirection;
+
+                if (type == PathTypes.loop)
+                {
+                    if (movingTo >= PathSeq.Length)
+                        movingTo = 0;
+                    
+                    if (movingTo < 0)
+                        movingTo = PathSeq.Length - 1;
+                    
+                }
+            }
+        } 
+    }
+
+}
